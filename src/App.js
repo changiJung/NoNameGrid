1186454f0d4b9657306d01312a97a90452541914
changi <changirang@gmail.com>
2022-04-03 20:33:36 +0900

    tree

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/Main.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+
+      <div className="mainTest">
+
+      
+
+        기본적인 명명규칙
+        <br></br>
+          - 폴더는 소문자로 작성
+          <br></br>
+          - 파일명은 파스칼 표기법으로 작성
+          <br></br>
+          - 폴더안에 기본 파일js 는 하나다만 작성
+          <br></br>
+          - 그 안에 폴더에 추가 js 파일 추가 작성
+          <br></br>
+        React v18 릴리즈 3.29로 인하여
+        <br></br>
+        해당기능을 도입해보면........?  
+        <br></br>
+      </div>        
+
+
+
       </header>
     </div>
   );
