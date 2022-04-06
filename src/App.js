5405d6e19ef693019723f29a49a2832ecaa7ab91
changi <changirang@gmail.com>
2022-04-06 23:10:12 +0900

    V2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header" >
 
-      <div className="mainTest">
+      {/* <div className="mainTest"> */}
 
+      <div className="mainTest">
       
 
         기본적인 명명규칙
@@ -17,7 +18,7 @@ function App() {
           <br></br>
           - 파일명은 파스칼 표기법으로 작성
           <br></br>
-          - 폴더안에 기본 파일js 는 하나다만 작성
+          - 폴더안에 기본 파일js 는 하나만 작성
           <br></br>
           - 그 안에 폴더에 추가 js 파일 추가 작성
           <br></br>
@@ -25,6 +26,14 @@ function App() {
         <br></br>
         해당기능을 도입해보면........?  
         <br></br>
+
+        suspense 추가
+        <br></br>
+        autobating 은........... .........?????? 
+        <br></br>
+        자동으로 설정인것 같은데 바꿔야 할 필요가 있을까?
+        
+
       </div>        
 
 
